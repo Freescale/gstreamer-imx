a2dd489e8e47268685d38f81ead8e99b6e483def
Carlos Rafael Giani <dv@pseudoterminal.org>
2014-05-06 22:46:11 +0200

    Enable GL prototypes for glTexDirectVIV and other direct texture functions
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

--- src/eglvivsink/gl_headers.h
+++ src/eglvivsink/gl_headers.h
@@ -2,6 +2,10 @@
 #define GST_IMX_EGL_VIV_SINK_GL_HEADERS_H
 
 
+#if !defined(GL_GLEXT_PROTOTYPES)
+# define GL_GLEXT_PROTOTYPES
+#endif
+
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <GLES2/gl2.h>
