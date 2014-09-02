a298a2ccbd79f0750197211ddf1754ea172b5c4d
Mark Nauwelaerts <mnauw@users.sourceforge.net>
2014-09-02 16:57:17 +0200

    common: phys_mem_meta: also copy meta when buffer and physical memory copied
    ... so as to support zero-copy buffer metadata copies in tee'd pipelines.

--- src/common/phys_mem_meta.c
+++ src/common/phys_mem_meta.c
@@ -25,6 +25,7 @@ static gboolean gst_imx_phys_mem_meta_init(GstMeta *meta, G_GNUC_UNUSED gpointer
 	GstImxPhysMemMeta *imx_phys_mem_meta = (GstImxPhysMemMeta *)meta;
 	imx_phys_mem_meta->phys_addr = 0;
 	imx_phys_mem_meta->padding = 0;
+	imx_phys_mem_meta->parent = NULL;
 	return TRUE;
 }
 
@@ -43,6 +44,58 @@ GType gst_imx_phys_mem_meta_api_get_type(void)
 	return type;
 }
 
+static gboolean gst_imx_phys_meta_transform (GstBuffer * dest, GstMeta * meta,
+	GstBuffer * buffer, GQuark type, gpointer data)
+{
+	GstImxPhysMemMeta *dmeta, *smeta;
+
+	smeta = (GstImxPhysMemMeta *) meta;
+
+	if (GST_META_TRANSFORM_IS_COPY(type)) {
+		GstMetaTransformCopy *copy = data;
+		gboolean do_copy = FALSE;
+
+		/* only copy if both buffers have 1 identical memory */
+		if (gst_buffer_n_memory(dest) == gst_buffer_n_memory(buffer) &&
+			gst_buffer_n_memory(dest) == 1) {
+			GstMemory *mem1, *mem2;
+
+			mem1 = gst_buffer_get_memory(dest, 0);
+			mem2 = gst_buffer_get_memory(buffer, 0);
+			if (mem1 == mem2)
+				do_copy = TRUE;
+			gst_memory_unref (mem1);
+			gst_memory_unref (mem2);
+		}
+
+		if (!copy->region && do_copy) {
+			/* only copy if the complete data is copied as well */
+			dmeta = (GstImxPhysMemMeta *)
+				gst_buffer_add_meta(dest, gst_imx_phys_mem_meta_get_info(), NULL);
+
+			if (!dmeta)
+				return FALSE;
+
+			GST_DEBUG ("copy phys metadata");
+			dmeta->phys_addr = smeta->phys_addr;
+			dmeta->padding = smeta->padding;
+			dmeta->x_padding = smeta->x_padding;
+			dmeta->y_padding = smeta->y_padding;
+			if (smeta->parent)
+				dmeta->parent = gst_buffer_ref(smeta->parent);
+			else
+			dmeta->parent = gst_buffer_ref(buffer);
+		}
+	}
+	return TRUE;
+}
+
+static void
+gst_imx_phys_meta_free (GstMeta * meta, G_GNUC_UNUSED GstBuffer * buffer)
+{
+	GstImxPhysMemMeta *smeta = (GstImxPhysMemMeta *) meta;
+	gst_buffer_replace(&smeta->parent, NULL);
+}
 
 GstMetaInfo const * gst_imx_phys_mem_meta_get_info(void)
 {
@@ -55,8 +108,8 @@ GstMetaInfo const * gst_imx_phys_mem_meta_get_info(void)
 			"GstImxPhysMemMeta",
 			sizeof(GstImxPhysMemMeta),
 			GST_DEBUG_FUNCPTR(gst_imx_phys_mem_meta_init),
-			(GstMetaFreeFunction)NULL,
-			(GstMetaTransformFunction)NULL
+			(GstMetaFreeFunction)(gst_imx_phys_meta_free),
+			(GstMetaTransformFunction)(gst_imx_phys_meta_transform)
 		);
 		g_once_init_leave(&gst_imx_phys_mem_meta_info, meta);
 	}
