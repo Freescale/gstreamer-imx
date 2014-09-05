9bc5f7367858493afe266e56648d2dad5babf253
Carlos Rafael Giani <dv@pseudoterminal.org>
2014-09-05 23:07:46 +0200

    Cleanup
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

--- src/common/phys_mem_meta.c
+++ src/common/phys_mem_meta.c
@@ -44,39 +44,39 @@ GType gst_imx_phys_mem_meta_api_get_type(void)
 	return type;
 }
 
-static gboolean gst_imx_phys_meta_transform (GstBuffer * dest, GstMeta * meta,
-	GstBuffer * buffer, GQuark type, gpointer data)
+static gboolean gst_imx_phys_meta_transform(GstBuffer *dest, GstMeta *meta, GstBuffer *buffer, GQuark type, gpointer data)
 {
 	GstImxPhysMemMeta *dmeta, *smeta;
 
-	smeta = (GstImxPhysMemMeta *) meta;
+	smeta = (GstImxPhysMemMeta *)meta;
 
-	if (GST_META_TRANSFORM_IS_COPY(type)) {
+	if (GST_META_TRANSFORM_IS_COPY(type))
+	{
 		GstMetaTransformCopy *copy = data;
 		gboolean do_copy = FALSE;
 
 		/* only copy if both buffers have 1 identical memory */
-		if (gst_buffer_n_memory(dest) == gst_buffer_n_memory(buffer) &&
-			gst_buffer_n_memory(dest) == 1) {
+		if ((gst_buffer_n_memory(dest) == gst_buffer_n_memory(buffer)) && (gst_buffer_n_memory(dest) == 1))
+		{
 			GstMemory *mem1, *mem2;
 
 			mem1 = gst_buffer_get_memory(dest, 0);
 			mem2 = gst_buffer_get_memory(buffer, 0);
 			if (mem1 == mem2)
 				do_copy = TRUE;
-			gst_memory_unref (mem1);
-			gst_memory_unref (mem2);
+			gst_memory_unref(mem1);
+			gst_memory_unref(mem2);
 		}
 
-		if (!copy->region && do_copy) {
+		if (!copy->region && do_copy)
+		{
 			/* only copy if the complete data is copied as well */
-			dmeta = (GstImxPhysMemMeta *)
-				gst_buffer_add_meta(dest, gst_imx_phys_mem_meta_get_info(), NULL);
+			dmeta = (GstImxPhysMemMeta *)gst_buffer_add_meta(dest, gst_imx_phys_mem_meta_get_info(), NULL);
 
 			if (!dmeta)
 				return FALSE;
 
-			GST_DEBUG ("copy phys metadata");
+			GST_DEBUG("copy phys metadata");
 			dmeta->phys_addr = smeta->phys_addr;
 			dmeta->padding = smeta->padding;
 			dmeta->x_padding = smeta->x_padding;
@@ -84,18 +84,20 @@ static gboolean gst_imx_phys_meta_transform (GstBuffer * dest, GstMeta * meta,
 			if (smeta->parent)
 				dmeta->parent = gst_buffer_ref(smeta->parent);
 			else
-			dmeta->parent = gst_buffer_ref(buffer);
+				dmeta->parent = gst_buffer_ref(buffer);
 		}
 	}
+
 	return TRUE;
 }
 
-static void
-gst_imx_phys_meta_free (GstMeta * meta, G_GNUC_UNUSED GstBuffer * buffer)
+
+static void gst_imx_phys_meta_free(GstMeta *meta, G_GNUC_UNUSED GstBuffer *buffer)
 {
-	GstImxPhysMemMeta *smeta = (GstImxPhysMemMeta *) meta;
+	GstImxPhysMemMeta *smeta = (GstImxPhysMemMeta *)meta;
 	gst_buffer_replace(&smeta->parent, NULL);
 }
+
 
 GstMetaInfo const * gst_imx_phys_mem_meta_get_info(void)
 {
@@ -108,8 +110,8 @@ GstMetaInfo const * gst_imx_phys_mem_meta_get_info(void)
 			"GstImxPhysMemMeta",
 			sizeof(GstImxPhysMemMeta),
 			GST_DEBUG_FUNCPTR(gst_imx_phys_mem_meta_init),
-			(GstMetaFreeFunction)(gst_imx_phys_meta_free),
-			(GstMetaTransformFunction)(gst_imx_phys_meta_transform)
+			GST_DEBUG_FUNCPTR(gst_imx_phys_meta_free),
+			GST_DEBUG_FUNCPTR(gst_imx_phys_meta_transform)
 		);
 		g_once_init_leave(&gst_imx_phys_mem_meta_info, meta);
 	}
