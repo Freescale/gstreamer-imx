3c421464d16308ac3fe19372c880d6c59eeeb01a
Carlos Rafael Giani <dv@pseudoterminal.org>
2014-09-16 20:41:19 +0200

    common: Add GstImxFDObject for reference counted file descriptors
    Useful if the same file descriptor is used in several places, to ensure
    it isn't closed prematurely
    
    Signed-off-by: Carlos Rafael Giani <dv@pseudoterminal.org>

---
+++ src/common/fd_object.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include "fd_object.h"
+
+
+GST_DEBUG_CATEGORY_STATIC(imx_fd_object_debug);
+#define GST_CAT_DEFAULT imx_fd_object_debug
+
+
+GType gst_imx_fd_object_api_get_type(void)
+{
+	static volatile GType type;
+	static gchar const *tags[] = { "resource", "filedecriptor", NULL };
+
+	if (g_once_init_enter(&type))
+	{
+		GType _type = gst_meta_api_type_register("GstImxFDObjectAPI", tags);
+		g_once_init_leave(&type, _type);
+
+		GST_DEBUG_CATEGORY_INIT(imx_fd_object_debug, "imxfdobject", 0, "File descriptor object");
+	}
+
+	return type;
+}
+
+
+static void gst_fd_object_free(GstImxFDObject *obj)
+{
+	if (obj->fd != -1)
+	{
+		GST_LOG("close FD %d in object %p", obj->fd, (gpointer)obj);
+		close(obj->fd);
+		obj->fd = -1;
+	}
+}
+
+
+static void gst_fd_object_init(GstImxFDObject *obj, int fd)
+{
+	gst_mini_object_init(
+		GST_MINI_OBJECT_CAST(obj),
+		0,
+		gst_imx_fd_object_api_get_type(),
+		NULL,
+		NULL,
+		(GstMiniObjectFreeFunction)gst_fd_object_free
+	);
+
+	obj->fd = fd;
+}
+
+
+GstImxFDObject* gst_fd_object_new(int fd)
+{
+	GstImxFDObject *obj;
+
+	obj = g_slice_new(GstImxFDObject);
+	GST_LOG("new %p with FD %d", (gpointer)obj, fd);
+
+	gst_fd_object_init(obj, fd);
+
+	return GST_IMX_FD_OBJECT_CAST(obj);
+}
---
+++ src/common/fd_object.h
@@ -0,0 +1,54 @@
+#ifndef GST_IMX_COMMON_FD_OBJECT_H
+#define GST_IMX_COMMON_FD_OBJECT_H
+
+#include <gst/gst.h>
+
+
+G_BEGIN_DECLS
+
+
+GST_EXPORT GType _gst_imx_fd_object_type;
+
+typedef struct _GstImxFDObject GstImxFDObject;
+
+#define GST_IMX_FD_OBJECT_TYPE                        (gst_imx_fd_object_api_get_type())
+#define GST_IMX_IS_FD_OBJECT(obj)                     (GST_IS_MINI_OBJECT_TYPE((obj), GST_IMX_FD_OBJECT_TYPE))
+#define GST_IMX_FD_OBJECT_CAST(obj)                   ((GstImxFDObject *)(obj))
+#define GST_IMX_FD_OBJECT(obj)                        (GST_IMX_FD_OBJECT_CAST(obj))
+
+
+/* This is a miniobject that handles the lifetime of a file descriptor
+ * with refcounting. Once the refcount reaches zero, the specified file
+ * descriptor is closed. Useful for resources that may be used by
+ * multiple entities, since then, the refcounting ensures the FD is
+ * closed only when all these entities are shut down.
+ */
+
+
+struct _GstImxFDObject
+{
+	GstMiniObject mini_object;
+	int fd;
+};
+
+
+static inline GstImxFDObject* gst_imx_fd_object_ref(GstImxFDObject *fd_object)
+{
+	return (GstImxFDObject *)gst_mini_object_ref(GST_MINI_OBJECT_CAST(fd_object));
+}
+
+
+static inline void gst_imx_fd_object_unref(GstImxFDObject *fd_object)
+{
+	gst_mini_object_unref(GST_MINI_OBJECT_CAST(fd_object));
+}
+
+
+GType gst_imx_fd_object_api_get_type(void);
+GstImxFDObject* gst_fd_object_new(int fd);
+
+
+G_END_DECLS
+
+
+#endif
